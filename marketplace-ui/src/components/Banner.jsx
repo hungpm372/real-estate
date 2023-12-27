e5ee41035d50228fb76f882762cdb47c7bd2c3ff
Phan Minh Hung <hungpm372@gmail.com>
2023-12-27 20:25:27 +0700

    component

---
+++ marketplace-ui/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import { memo } from 'react';
+import { Container } from 'reactstrap';
+
+const Banner = () => {
+    return (
+        <Container fluid className='banner'>
+        </Container>
+    )
+};
+
+export default memo(Banner);
